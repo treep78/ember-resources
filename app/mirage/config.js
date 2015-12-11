b54def47259b9aa4170509ea5ddde88dc962be96
Matt Brendzel <matt.brendzel@generalassemb.ly>
2015-12-11 13:50:39 -0500

    Adds fixtures and `ember-data` setup (Model + Adapter) for 'item' resource.

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,11 +1,18 @@
 export default function() {
 
   this.namespace = 'api';
+
   this.get('/pokemon');
   this.get('/pokemon/:id');
   this.post('/pokemon');
   this.put('/pokemon/:id');
   this.del('/pokemon/:id');
+
+  this.get('/items');
+  this.get('/items/:id');
+  this.post('/items');
+  this.put('/items/:id');
+  this.del('/items/:id');
 
 
   // These comments are here to help you get started. Feel free to delete them.
