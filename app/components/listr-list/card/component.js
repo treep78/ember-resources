b494f0139ee0646a40ad6b228052722a57ddac89
Antony Donovan <antony.donovan@generalassemb.ly>
2016-09-29 16:26:03 -0400

    Refactor location of isEmpty property
    from component `listr-list/card`
    to model `list`
    
    Improve separation of concerns by keeping model concerns in the model

--- app/components/listr-list/card/component.js
+++ app/components/listr-list/card/component.js
@@ -1,10 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isEmpty: Ember.computed('list', function () {
-    let items = this.get('list').hasMany('items');
-    return items.ids().length === 0;
-  }),
   actions: {
     edit () {
       this.sendAction('edit', this.get('list'));
--- app/list/model.js
+++ app/list/model.js
@@ -1,7 +1,12 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   hidden: DS.attr('boolean'),
   items: DS.hasMany('item'),
+  isEmpty: Ember.computed('items', function () {
+    let items = this.hasMany('items');
+    return items.ids().length === 0;
+  }),
 });
