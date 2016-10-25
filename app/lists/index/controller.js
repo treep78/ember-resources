374f0c5e8faf9132cf8418dda5d1eca150ea56d8
Antony Donovan <antony.donovan@generalassemb.ly>
2016-10-24 21:46:57 -0400

    Add lookahead to pagination
    Don't show next link if next page would be empty.

--- app/lists/index/controller.js
+++ app/lists/index/controller.js
@@ -1,15 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['page', 'size'],
-  size: 2,
+  queryParams: ['page', 'size', 'lookahead'],
   page: 1,
+  size: 2,
+  lookahead: true,
   prev: Ember.computed('page', function () {
     let page = this.get('page');
     return page > 1 && page - 1;
   }),
   next: Ember.computed('page', function () {
-    return this.get('model.length') >= this.get('size') &&
+    return this.get('model.length') > this.get('size') &&
       this.get('page') + 1;
+  }),
+
+  modelSlice: Ember.computed('size', 'model', function () {
+    let size = this.get('size');
+    let length = this.get('model.length');
+    let lists = this.get('model');
+
+    if (size < length) {
+      return lists.slice(0, -1);
+    } else {
+      return lists;
+    }
   }),
 });
