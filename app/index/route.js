5cc01f5c46d5d3d8374d12834c165190101442e5
treep78 <treep78@gmail.com>
2017-01-09 10:24:48 -0500

    ad route to index

--- app/index/route.js
+++ app/index/route.js
@@ -1,23 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model () {
-    return [
-      {
-        title: 'Favorites',
-        items: [
-          { content: 'Cats' },
-          { content: 'Dogs' },
-          { content: 'Scotch' },
-        ],
-      }, {
-        title: 'Todos',
-        items: [
-          { content: 'Learn Ember' },
-          { content: 'Change Oil' },
-          { content: 'Visit Portland' },
-        ],
-      },
-    ];
-  },
+
 });
