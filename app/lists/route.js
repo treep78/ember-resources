2398fb774daca99f7ef4e014cf3af64418ff9ac9
treep78 <treep78@gmail.com>
2017-01-09 11:31:35 -0500

    Add API integrations change lists view to card comp
    Adds API interation to persist changes and make requests from our backend rather than hardcoding the data in the model hook. Adds use of ember-data's store and creates model for 'list'.
    
    Refactiored the '/lists' view to use the listr-list/card component for summary information (and to use later)

--- app/lists/route.js
+++ app/lists/route.js
@@ -2,22 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    return [
-      {
-        title: 'Favorites',
-        items: [
-          { content: 'Cats' },
-          { content: 'Dogs' },
-          { content: 'Scotch' },
-        ],
-      }, {
-        title: 'Todos',
-        items: [
-          { content: 'Learn Ember' },
-          { content: 'Change Oil' },
-          { content: 'Visit Portland' },
-        ],
-      },
-    ];
+    return this.get('store').findAll('list');
   },
 });
